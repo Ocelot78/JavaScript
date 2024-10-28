33775b6118c1a045b1cb74b0a42e70ec1dc76826
Ocelot78 <wojtekkonieczny1@gmail.com>
2024-10-28 21:38:54 +0100

    Starting a new project

---
+++ First steps/Calculator/Calculator.js
@@ -0,0 +1,9 @@
+function przyciski(id) {
+    var poleoblicz = document.getElementById("Kalk");
+    poleoblicz.value += id
+    
+}
+function usuwanie() {
+    var poleusun = document.getElementById("Kalk")
+    poleusun.value = ""
+}
