1ea7fb64ee81638785226c458a9e47ab07cfcbc6
Ocelot78 <wojtekkonieczny1@gmail.com>
2024-11-02 15:01:46 +0100

    Calculator calculates

--- First steps/Calculator/Calculator.js
+++ First steps/Calculator/Calculator.js
@@ -1,16 +1,29 @@
 let usun = false
 let przycisk = false
+let znak = false
 let liczba1 = "empty";
 let liczba2 = "empty";
+let wartosc = ""
+let operator = ""
+let wynik = 0
+
 function przyciski(id) {
-    var poleoblicz = document.getElementById("Kalk");
+    let poleoblicz = document.getElementById("Kalk");
     usun = false
     poleoblicz.value += id
     przycisk = true
+    let znaki = ["+","-","*","/"]
+    if (znaki.includes(id)) {
+        znak = true
+        operator = id
+    } //else {
+        //znak = false
+    //}
+    console.log("znak "+znak)
     
 }
 function usuwanie() {
-    var poleusun = document.getElementById("Kalk")
+    let poleusun = document.getElementById("Kalk");
     usun = true
     poleusun.value = ""
     //console.log(usun)
@@ -22,18 +35,50 @@ function pobieranie_liczby(id) {
         liczba1 = "empty";
         liczba2 = "empty";
         //console.log("reset")
-    }
-    if (przycisk == true) {
-        wartosc = document.getElementById("Kalk").value;
-        wartosc = id;
-    }
-    if (liczba1 == "empty") {
-        liczba1 = wartosc;
-    } else {
-        liczba1 += wartosc ;
+        znak = false
+        usun = false
     }
 
+    //wartosc = document.getElementById("Kalk").value;
+
+    if (znak == false) {
+        if (przycisk == true) {
+            wartosc = id;
+        }
+        if (liczba1 == "empty") {
+            liczba1 = wartosc;
+        } else {
+            liczba1 += wartosc ;
+        }
+    } else {
+        if (przycisk == true) {
+            wartosc = id;
+        }
+        if (liczba2 == "empty") {
+            liczba2 = wartosc
+        } else {
+            liczba2 += wartosc
+        }
+    }
+ 
+
     //console.log(usun);
-    //console.log(liczba1);
-    //console.log(liczba2);
+    console.log("liczba1 " +liczba1);
+    console.log("liczba2 " +liczba2);
+}
+function obliczanie() {
+    let polewynik = document.getElementById("Kalk");
+    liczba1 = parseFloat(liczba1);
+    liczba2 = parseFloat(liczba2);
+    if (operator == "+"){
+        wynik = liczba1 + liczba2;
+    } else if (operator == "-") {
+        wynik = liczba1 - liczba2;
+    } else if (operator == "*") {
+        wynik = liczba1 * liczba2;
+    } else if (operator == "/") {
+        wynik = liczba1 / liczba2;
+    }   
+    polewynik.value = wynik.toString();
+    console.log(wynik);
 }
