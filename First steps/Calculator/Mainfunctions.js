676cd0bfc3bbb9cdbfd53611dd19aa9ef1f0e8a9
Ocelot78 <wojtekkonieczny1@gmail.com>
2024-11-10 17:42:42 +0100

    created a Plus minus improved code

--- First steps/Calculator/Mainfunctions.js
+++ First steps/Calculator/Mainfunctions.js
@@ -6,7 +6,7 @@ let liczba2 = "empty";
 let wartosc = ""
 let operator = ""
 let wynik = 0
-
+let plusminusPF = false
 function przyciski(id) {
     let poleoblicz = document.getElementById("Kalk");
     let wpis = poleoblicz.value 
@@ -14,10 +14,13 @@ function przyciski(id) {
     przycisk = true
     let znaki = ["+","-","*",":","^","%"]
 
-    if (operator && znaki.includes(id)) {
-        poleoblicz.value = wpis.slice(0, -1); 
-        operator = id; 
-        poleoblicz.value += id;  
+    if (znaki.includes(id)) {
+        if (znak) {
+            poleoblicz.value = wpis.slice(0, -1); 
+        }
+        poleoblicz.value += id;
+        operator = id;
+        znak = true;
     } else {
         if (znaki.includes(id)) {
             znak = true
@@ -91,7 +94,7 @@ function obliczanie() {
     } else if (operator == "/" && liczba2 == 0 ){
         dzialanie.value = liczba1 + operator + liczba2 + "="
         polewynik.value="Nie można dzielić przez zero"
-    } else {
+    } else if (plusminusPF == false){
         dzialanie.value = liczba1 + operator + liczba2 + "="
         polewynik.value = wynik.toString();
         console.log(liczba1);
@@ -100,8 +103,51 @@ function obliczanie() {
         liczba1 = wynik
         liczba2 = "empty"
         znak = false
+    } else if (plusminusPF == true) {
+        dzialanie.value = liczba1.toString() + liczba2.toString() + "="
+        polewynik.value = wynik.toString();
+        console.log(liczba1);
+        console.log(liczba2);
+        console.log(wynik);
+        liczba1 = wynik
+        liczba2 = "empty"
+        znak = false 
     }
 }
+function plusminus() {
+    let polewynik = document.getElementById("Kalk");
+
+    if (znak== false  && !isNaN(liczba1)) {
+        liczba1 = -liczba1 
+        liczba1 = parseFloat(liczba1); 
+        console.log(liczba1)
+        polewynik.value = liczba1.toString()
+    } else if (znak == true && !isNaN(liczba2)){
+        if (operator == "+") {
+            polewynik.value = polewynik.value - liczba2
+            liczba2 = parseFloat(liczba2);
+            liczba2 = -liczba2
+            console.log(liczba1)
+            console.log(liczba2)
+            polewynik.value = liczba1.toString() + liczba2.toString() 
+            if (plusminusPF == false) {
+                plusminusPF = true
+            } else {
+                plusminusPF = false
+            }
+        } else{
+            polewynik.value = polewynik.value - liczba2
+            liczba2 = parseFloat(liczba2);
+            liczba2 = -liczba2
+            console.log(liczba1)
+            console.log(liczba2)
+            polewynik.value = liczba1.toString() + operator + liczba2.toString()   
+        }
+   
+    }
+    console.log(plusminusPF)
+}
+//Dodatkowe funkcje kalkulatora
 function pierwiastek() {
     let dzialanie = document.getElementById("dzialanie");
     let polewynik = document.getElementById("Kalk");
